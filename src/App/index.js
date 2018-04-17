97533d50bd722c4bceb23983cd20d555569cac26
Jared Griffin <griffinjared1388@gmail.com>
2018-04-17 15:50:34 -0500

    Refactored format of todolist to be a tree instead of a list

--- src/App/index.js
+++ src/App/index.js
@@ -20,28 +20,98 @@ class App extends Component {
     }
 
     componentDidMount() {
-        this.readTodoList();        
+        this.readTodoList();
     }
 
     readTodoList() {
         fs.readFile(`${app.getAppPath()}/todo`, (error, data) => {
             const listElements = data.toString('utf-8').split('\n').filter(line => line.trim().length > 0);
 
-            let parsedList = [];
-            let key = 0;
+            const parsedList = [];
+            let key = 1;
 
-            for (let i = 0; i < listElements.length; i++) {
-                const currentLine = listElements[i];
+            // for (let i = 0; i < listElements.length; i++) {
+            //     const currentLine = listElements[i];
 
-                if (currentLine.charAt(0) === ':') {
-                    parsedList.push({ title: currentLine, elements: [] });
-                    key++;
-                } else
-                    parsedList[key - 1].elements.push(currentLine);
-            }
+            //     if (currentLine.charAt(0) === ':') {
+            //         parsedList.push({ title: currentLine, elements: [] });
+            //         key++;
+            //     } else
+            //         parsedList[key - 1].elements.push(currentLine);
+            // }
+
+            let parentIds = [0];
+            let currentLevel = 0;
+
+            listElements.forEach(element => {
+                let level = 0;
+                while (element.charAt(level) == ' ')
+                    level++;
+
+                level = level / 4 + 1;
+
+                if (level > currentLevel) {
+                    parsedList.push({
+                        id: key,
+                        parentId: parentIds[parentIds.length - 1],
+                        value: element,
+                        children: []
+                    });
+
+                    currentLevel++;
+                    parentIds.push(key);
+                } else if (level === currentLevel) {
+                    parentIds.pop();
+                    parsedList.push({
+                        id: key,
+                        parentId: parentIds[parentIds.length - 1],
+                        value: element,
+                        children: []
+                    });
+
+                    parentIds.push(key);
+                } else {
+                    while (currentLevel > level) {
+                        currentLevel--;
+                        parentIds.pop();
+                    }
+
+                    parentIds.pop();
+                    parsedList.push({
+                        id: key,
+                        parentId: parentIds[parentIds.length - 1],
+                        value: element,
+                        children: []
+                    });
+
+                    parentIds.push(key);
+                }
+
+                key++;
+            });
+
+            const getTree = (list, parentId) => {
+                const tree = [];
+
+                for (let element in list) {
+                    if (list[element].parentId === parentId) {
+                        const children = getTree(list, list[element].id);
+
+                        if (children.length)
+                            list[element].children = children;
+
+                        tree.push(list[element]);
+                    }
+                }
+
+                return tree;
+            };
+
+            const treeifiedList = getTree(parsedList, 0);
+            console.log(treeifiedList);
 
             this.setState({
-                todoList: parsedList,
+                todoList: treeifiedList,
                 todoListRead: true
             });
         });
